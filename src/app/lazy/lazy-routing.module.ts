4c5a245cc99f376335ffcd688bc071b55edb550e
João Gabriel <j.gabrieltec@outlook.com>
2019-05-03 23:34:47 -0300

    Add form bootstrap .

--- src/app/lazy/lazy-routing.module.ts
+++ src/app/lazy/lazy-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SobreComponent } from './sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: SobreComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
+
 export class LazyRoutingModule { }
--- src/app/lazy/lazy.module.ts
+++ src/app/lazy/lazy.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { LazyRoutingModule } from './lazy-routing.module';
+import { SobreComponent } from './sobre/sobre.component';
 
 @NgModule({
-  declarations: [],
   imports: [
     CommonModule,
     LazyRoutingModule
-  ]
+  ],
+
+  declarations: [SobreComponent]
 })
+
 export class LazyModule { }
